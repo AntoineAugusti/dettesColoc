79988e3381d9572b27c12e6cd71bf067419dd6cb
Antoine Augusti <antoine@augusti.fr>
2014-04-18 14:24:06 +0200

    Renommage du dossier et ajout des fichiers SQL fakes

---
+++ livrables/fakeTables/permissions.sql
@@ -0,0 +1,14 @@
+DROP TABLE `permissions`;
+
+CREATE TABLE `permissions` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id` mediumint,
+  `nom` TEXT default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+INSERT INTO `permissions` (`id`,`nom`) VALUES (1,"creerMembre");
+INSERT INTO `permissions` (`id`,`nom`) VALUES (2,"supprimerMembre");
+INSERT INTO `permissions` (`id`,`nom`) VALUES (3,"creerEvenement");
+INSERT INTO `permissions` (`id`,`nom`) VALUES (4,"ajouterMateriel");
+INSERT INTO `permissions` (`id`,`nom`) VALUES (5,"reserverMateriel");
