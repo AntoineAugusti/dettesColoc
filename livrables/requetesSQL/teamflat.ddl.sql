db11e2fd4426c6d0204182c4efc8657fcb29001e
Antoine Augusti <antoine@augusti.fr>
2014-05-06 18:01:17 +0200

    Modification des requêtes SQL suite à la nouvelle version du CdC

--- livrables/requetesSQL/teamflat.ddl.sql
+++ livrables/requetesSQL/teamflat.ddl.sql
@@ -1,5 +1,5 @@
+-- ORGANISATEURS
 DROP TABLE IF EXISTS `organisateurs`;
-
 CREATE TABLE `organisateurs` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
   `idAdherent` mediumint,
@@ -8,19 +8,27 @@ CREATE TABLE `organisateurs` (
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1;
 
-DROP TABLE IF EXISTS `permissions`;
-
-CREATE TABLE `permissions` (
+-- ROLES
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
   `nom` TEXT default NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1;
 
-DROP TABLE IF EXISTS `permissions_organisateurs`;
-
-CREATE TABLE `permissions_organisateurs` (
+-- ROLES_ORGANISATEURS
+DROP TABLE IF EXISTS `roles_organisateurs`;
+CREATE TABLE `roles_organisateurs` (
   `id` mediumint(8) unsigned NOT NULL auto_increment,
   `id_organisateur` mediumint default NULL,
-  `id_permission` mediumint default NULL,
+  `id_role` mediumint default NULL,
+  PRIMARY KEY (`id`)
+) AUTO_INCREMENT=1;
+
+-- DIRIGE_ROLE
+CREATE TABLE `dirige_role` (
+  `id` mediumint(8) unsigned NOT NULL auto_increment,
+  `id_role_dirigeant` mediumint default NULL,
+  `id_role_dirige` mediumint default NULL,
   PRIMARY KEY (`id`)
 ) AUTO_INCREMENT=1;
--- livrables/requetesSQL/teamflat.requetes.sql
+++ livrables/requetesSQL/teamflat.requetes.sql
@@ -1,15 +1,36 @@
 -- Vérification des permissions de l'organisatrice "Cheyenne"
-SELECT p.nom
-FROM permissions p
-JOIN permissions_organisateurs po ON po.id_permission = p.id
-JOIN organisateurs o ON o.id = po.id_organisateur
-WHERE o.login =  'Cheyenne'
+SELECT r.nom
+FROM roles r
+JOIN roles_organisateurs ro ON ro.id_role = r.id
+JOIN organisateurs o ON o.id = ro.id_organisateur
+WHERE o.login =  'Cheyenne';
 
 -- Vérification du mot de passe de l'organisatrice "Cheyenne"
 SELECT * FROM organisateurs WHERE login = 'Cheyenne' AND motdepasse = 'JYN95PHY4GV';
 
 -- Création d'un nouvel organisateur
 INSERT INTO organisateurs (idAdherent, login, motdepasse) VALUES (341, "Grosbatise", "QFF00TLS3XE");
-INSERT INTO permissions_organisateurs (id_organisateur, id_permission) VALUES (101, 1);
-INSERT INTO permissions_organisateurs (id_organisateur, id_permission) VALUES (101, 2);
-INSERT INTO permissions_organisateurs (id_organisateur, id_permission) VALUES (101, 3);
+INSERT INTO roles_organisateurs (id_organisateur, id_role) VALUES (101, 1);
+INSERT INTO roles_organisateurs (id_organisateur, id_role) VALUES (101, 2);
+INSERT INTO roles_organisateurs (id_organisateur, id_role) VALUES (101, 3);
+
+-- Vérification des rôles dirigés par le "Président"
+SELECT r2.nom
+FROM roles r2
+JOIN dirige_role d ON r2.id = d.id_role_dirige
+JOIN roles r ON r.id = d.id_role_dirigeant
+WHERE r.nom = 'president';
+
+-- Vérification des organisateurs dirigés par le "Président"
+SELECT ro.id_organisateur, o.login, r.nom
+FROM organisateurs o
+JOIN roles_organisateurs ro ON ro.id_organisateur = o.id
+JOIN roles r ON ro.id_role = r.id
+WHERE ro.id_role IN (
+	SELECT r2.id
+	FROM roles r2
+	JOIN dirige_role d ON r2.id = d.id_role_dirige
+	JOIN roles r ON r.id = d.id_role_dirigeant
+	WHERE r.nom = 'president'
+)
+ORDER BY ro.id_organisateur ASC;
